142c3cb5ead133913ed85bd6f9f6311ca75f18a6
Geraldo de Paula <87886511+gsennin@users.noreply.github.com>
2022-04-28 00:06:05 -0300

    Calc

---
+++ calc.js
@@ -0,0 +1,17 @@
+/* 
+Módulos JS 
+ Crio minhas funcões separadamente
+ module.exports para enviar o modulo para o arquivo principal
+ Recebo no arquivo principal usando require("./") dentro de uma varíavel.
+*/
+
+var FuncaoSoma = require("./soma");
+var FuncaoSub = require("./sub");
+var FuncaoMult = require("./mult");
+var FuncaoDiv = require("./div");
+
+// Print de uma string contatenando com a Função e dentro de parenteses as operaçoes
+console.log("Resultado de sua soma é = ", FuncaoSoma(10,20));
+console.log("Resultado de sua subtração é = ", FuncaoSub(20,10));
+console.log("Resultado de sua multiplicação é = ", FuncaoMult(10,10));
+console.log("Resultado de sua divisão é = ", FuncaoDiv(10,2));
---
+++ soma.js
@@ -0,0 +1,6 @@
+var soma = function(a,b){
+return a+b
+}
+
+//module.exports para enviar o modulo para o arquivo principal
+module.exports = soma;
