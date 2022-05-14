a5730a2b7df240a6da26634a8f8f08b8eca0c2ad
Geraldo de Paula <87886511+gsennin@users.noreply.github.com>
2022-05-14 12:27:44 -0300

    Curso cod3r - Function

---
+++ funcoes_curso_coder.js
@@ -0,0 +1,29 @@
+// Function declaration
+//Sem parametro e não retorna nada 
+function sayHello() {
+    console.log('Hello!')
+}
+
+sayHello()
+
+// Com parametro e não retorna nada
+function sayHelloTo(name) {
+    console.log(`Hello ${name}!`)
+}
+
+sayHelloTo('Mike')
+
+// Não recebe parametro mas retorna algo
+function returnHi(){
+    return 'Hi!'
+}
+
+const ola = returnHi()
+console.log(returnHi())
+
+//Com parametro e com retorno
+function returnHiTo(name) {
+    return `Hi ${name}!`
+}
+
+console.log(returnHiTo('Geraldo'))
