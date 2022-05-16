285c6591f6717744dcb41bec7fdaf8ecdcee5776
Geraldo de Paula <87886511+gsennin@users.noreply.github.com>
2022-05-16 14:25:04 -0300

    Add files via upload

---
+++ concatenando_array.js
@@ -0,0 +1,9 @@
+// Concatenando Arrays
+// Ex: 2 salas de aulas, vamos juntar elas em uma só!
+const salaDePython = ["Geraldo", "Bia", "Genesis"]
+const salaDeJs = ["Ronaldo", "Guilherme", "Roberto"]
+
+const salaUnificada = salaDePython.concat(salaDeJs)
+                                //.concat( )
+                                // array1.concat(array2)
+console.log(salaUnificada)
