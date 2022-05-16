5a4948b06f91e1b513e1ab61324fd3620a342538
Geraldo de Paula <87886511+gsennin@users.noreply.github.com>
2022-05-16 14:24:36 -0300

    Add files via upload

---
+++ array_2_dimensoesjs.js
@@ -0,0 +1,9 @@
+// Array de 2 dimensões também chamada de Matrizes -> Uma lista com listas
+//             [0]     
+const alunos = ['Geraldo', 'Bia', "ABC"]
+//                     [0]
+const mediaDosAlunos = [10,9,7]
+// Variavel de lista de listas
+let listaDeMediaEAlunos = [alunos, mediaDosAlunos]
+//                [alunos][mediaDosAlunos]
+console.log( ` ${listaDeMediaEAlunos[0][0]} sua média é ${listaDeMediaEAlunos[1][0]}`)
