7d070d5245f32c6a960cc1d64520c92d35cb6c26
Geraldo de Paula <87886511+gsennin@users.noreply.github.com>
2022-05-20 15:36:41 -0300

    Add files via upload

---
+++ for.js
@@ -0,0 +1,8 @@
+const listaDeItens = [100, 200, 300, 400, 500, 600]
+// i = indice vai começar no 0
+// i < listaDeItens.length; É a condição a lista vai de 0 a 5
+// i++ Vai somar o i + 1 
+for (let i = 0; i < listaDeItens.length; i++){
+    console.log(i, listaDeItens[i])
+    //Imprime o i atual e em qual está.
+}
