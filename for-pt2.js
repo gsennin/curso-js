1ec55eb6e158aa1c578279b11ee9ba97ffc80f14
Geraldo de Paula <87886511+gsennin@users.noreply.github.com>
2022-05-23 14:22:25 -0300

    Add files via upload

---
+++ for-pt2.js
@@ -0,0 +1,11 @@
+const notas = [10, 6, 7, 5]
+// importante declarar a somaDasNotas fora do For
+let somaDasNotas = 0
+
+for ( let i = 0; i < notas.length ; i++){
+// += soma a variavel da direito na esquerda.    
+    somaDasNotas += notas[i];
+}
+
+let media = somaDasNotas / notas.length
+console.log(media)
