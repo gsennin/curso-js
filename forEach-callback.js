f961d7c2371ffb7e1b4133bfde850e2db28b5070
Geraldo de Paula <87886511+gsennin@users.noreply.github.com>
2022-06-01 12:03:27 -0300

    Update forEach-callback.js

--- forEach-callback.js
+++ forEach-callback.js
@@ -1,5 +1,5 @@
 const nomesDosAlunos = ["Geraldo", "Guilherme", "Pedro", "Jõao"]
-// O forEach recebe uma função e depois posso colocar o indece
+// O forEach recebe uma função e depois posso colocar o indice
 nomesDosAlunos.forEach(imprimeAlunos)
 //Criar a função aqui fora #A função sempre recebe um parametro
 function imprimeAlunos(nomes){
