0427a32aa1687edee92fa0cd4a209ad358e2cd91
Geraldo de Paula <87886511+gsennin@users.noreply.github.com>
2022-06-02 15:08:47 -0300

    Update java-reduce.js

--- java-reduce.js
+++ java-reduce.js
@@ -10,3 +10,12 @@ function mediaSala(notasDaSala){
 console.log(`Média da sala de JS ${mediaSala(salaJs)}`)
 console.log(`Média da sala de Java ${mediaSala(salaJava)}`)
 console.log(`Média da sala de Python ${mediaSala(salaPython)}`)
+
+/* 1) O método reduce() está trabalhando com dois parâmetros. 
+O primeiro é a função callback obrigatória para retornar o cálculo e o segundo parâmetro é um número que representa o valor inicial - no caso, 0.
+
+2) A função callback foi escrita diretamente dentro do reduce(), e esta função também está recebendo dois parâmetros, ambos obrigatórios: 
+O valor acumulado e o valor atual.
+
+3) A função callback foi escrita na forma de arrow function; nesse caso,
+quando só temos uma linha de instrução dentro da função (atual + acum) não precisamos usar chaves e nem da palavra-chave return. */
