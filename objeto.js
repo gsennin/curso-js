fb4bead949046ddfdd0e8acf2ad698bbeeb5831c
Geraldo de Paula <87886511+gsennin@users.noreply.github.com>
2022-06-07 13:32:54 -0300

    Add files via upload

---
+++ objeto.js
@@ -0,0 +1,11 @@
+// Diferenciando uma Array de um Objeto
+// È valido ter uma lista de alguma coisa específica lista de nomes, numeros, notas etc
+const umaArray = [ "Geraldo", "Beatriz", "Guilherme"]
+// Já o objetivo podemos colocar mais dados.
+const cliente = {
+    nome: "Geraldo",
+    idade: 26,
+    CPF: "123456789",
+    email: "geraldo@email.com",
+}
+console.log(cliente);
